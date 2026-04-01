403132fb338dc9e66ccb8c6afcb4304ff5d581fc
ablas123 <67266977+ablas123@users.noreply.github.com>
2026-04-01 19:01:24 +0200

    Refactor database connection configuration

--- database/config.php
+++ database/config.php
@@ -5,11 +5,10 @@ $user = getenv('DB_USER') ?: '4Q3JgFwtV72XTzm.root';
 $pass = getenv('DB_PASS') ?: 'hCCzJBE3Ko33HuTW';
 $dbname = getenv('DB_NAME') ?: 'hospital_db';
 
-// SSL configuration
-$ssl_ca = __DIR__ . '/ca.pem';  // مسار الشهادة
+$ssl_ca = __DIR__ . '/ca.pem';   // الملف موجود في نفس المجلد (database)
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL); // تعيين SSL
+mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
 if (!mysqli_real_connect($conn, $host, $user, $pass, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("Connection failed: " . mysqli_connect_error());
 }
